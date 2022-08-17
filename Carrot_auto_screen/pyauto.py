64dd1db5afe0bf5098719e42c123a40dd4998974
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-08-17 09:33:59 +0900

    Create pyauto.py

---
+++ Carrot_auto_screen/pyauto.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+import random
+
+while True:
+    pyautogui.FAILSAFE = True
+    screenW, screenH = pyautogui.size()
+    temp_x, temp_y = pyautogui.position()
+    time.sleep(250)
+    current_x, current_y = pyautogui.position()
+    if temp_x == current_x and temp_y == current_y:
+        ran_w = random.randint(1, screenW)
+        ran_h = random.randint(1, screenH)
+
+        pyautogui.moveTo(ran_w, ran_h, 0.3)
+        pyautogui.typewrite("    ", 1)
