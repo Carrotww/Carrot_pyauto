316e4642c5c2c36ebf279100de9a6f8151ee68ff
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-08-22 16:59:10 +0900

    Update Carrot_auto_screen 1.1.py

--- Carrot_auto_screen/Carrot_auto_screen 1.1.py
+++ Carrot_auto_screen/Carrot_auto_screen 1.1.py
@@ -3,30 +3,58 @@ import time
 import random
 import sys
 import psutil
+import tkinter as tk
 
-for proc in psutil.process_iter():
-    ps_name = proc.name()
-    cmdline = proc.cmdline()
+status = 1
 
-for proc in psutil.process_iter():
-  ps_name = proc.name()
-  if ps_name == 'python3':
-    cmdline = proc.cmdline()
-    print(cmdline)
+def button_start(val):
+    print("start")
+    global status
+    status = 1
 
-status = input("if input ""exit"", going shutdown : ")
+    while True:
+        pyautogui.FAILSAFE = True
+        screenW, screenH = pyautogui.size()
+        temp_x, temp_y = pyautogui.position()
+        if status == False:
+            break
+        time.sleep(145)
+        current_x, current_y = pyautogui.position()
+        if temp_x == current_x and temp_y == current_y:
+            ran_w = random.randint(1, screenW)
+            ran_h = random.randint(1, screenH)
 
-while True:
-    pyautogui.FAILSAFE = True
-    screenW, screenH = pyautogui.size()
-    temp_x, temp_y = pyautogui.position()
-    time.sleep(145)
-    current_x, current_y = pyautogui.position()
-    if temp_x == current_x and temp_y == current_y:
-        ran_w = random.randint(1, screenW)
-        ran_h = random.randint(1, screenH)
+            pyautogui.moveTo(ran_w, ran_h, 0.3)
+            pyautogui.typewrite(" ", 1)
+            # print(temp_x, temp_y)
+            # print(current_x, current_y)
 
-        pyautogui.moveTo(ran_w, ran_h, 0.3)
-        pyautogui.typewrite(" ", 1)
-        # print(temp_x, temp_y)
-        # print(current_x, current_y)
+def button_stop(val):
+    global status
+    print('system stop')
+    status = 0
+    return status
+
+def button_shutdown(val):
+    global status
+    print('system halt')
+    status = 0
+    time.sleep(4)
+    sys.exit(0)
+
+win = tk.Tk()
+win.title('hyeongseok auto screen')
+tk.Button(win, text = 'start', width=50, height=3, command= lambda cmd=status: button_start(cmd)).grid(column=0, row=1, columnspan=4)
+tk.Button(win, text = 'stop', width=50, height=3, command= lambda cmd=status: button_start(cmd)).grid(column=0, row=2, columnspan=4)
+tk.Button(win, text = 'shutdonw', width=50, height=3, command= lambda cmd=status: button_start(cmd)).grid(column=0, row=3, columnspan=4)
+win.mainloop()
+
+# for proc in psutil.process_iter():
+#     ps_name = proc.name()
+#     cmdline = proc.cmdline()
+#
+# for proc in psutil.process_iter():
+#   ps_name = proc.name()
+#   if ps_name == 'python3':
+#     cmdline = proc.cmdline()
+#     print(cmdline)
