7d2b88bddbf2e3d45068206d6643995477e53d57
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-27 17:12:25 +0900

    왜 안될까...

--- Carrot_crawling/main.py
+++ Carrot_crawling/main.py
@@ -3,13 +3,17 @@ from bs4 import BeautifulSoup
 import requests
 
 url = "https://ahnlab.recruiter.co.kr/app/jobnotice/list"
+url2 = "https://career.programmers.co.kr/job?page=10&min_career=0&order=recent"
 
-response = requests.get(url)
+response = requests.get(url2)
 
 if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
     title = soup.select_one('#divJobnoticeList > ul > li:nth-child(1) > div:nth-child(2) > h2 > span > a')
-    print(title)
+    title2 = soup.select('#list-positions-wrapper > ul > li:nth-child(1) > div.item-body > ul.list-position-tags')
+    for ti in title2:
+        print(ti)
+    print(title2)
 else:
     print(response.status_code)
